563c0fdbdf8b25b15085967da0f7099450f4e93d
Elliott Shugerman <eeshugerman@gmail.com>
2020-05-28 23:29:13 -0600

    don't use --insecure flag on curl download

--- src/install.sh
+++ src/install.sh
@@ -17,7 +17,7 @@ pip3 install awscli
 
 # install go-cron
 apk add curl
-curl -L --insecure https://github.com/odise/go-cron/releases/download/v0.0.6/go-cron-linux.gz | zcat > /usr/local/bin/go-cron
+curl -L https://github.com/odise/go-cron/releases/download/v0.0.6/go-cron-linux.gz | zcat > /usr/local/bin/go-cron
 chmod u+x /usr/local/bin/go-cron
 apk del curl
 
