5f68eb82b36663e5c11e0df5c8e2d9974ed4255a
Elliott Shugerman <eeshugerman@gmail.com>
2022-09-25 13:57:55 -0400

    minor typos

--- src/backup.sh
+++ src/backup.sh
@@ -39,12 +39,12 @@ if [ -n "$BACKUP_KEEP_DAYS" ]; then
   date_from_remove=$(date -d "@$(($(date +%s) - sec))" +%Y-%m-%d)
   backups_query="Contents[?LastModified<='${date_from_remove} 00:00:00'].{Key: Key}"
 
-  echo "Removing old backup from $S3_BUCKET..."
+  echo "Removing old backups from $S3_BUCKET..."
   aws $aws_args s3api list-objects \
     --bucket "${S3_BUCKET}" \
     --prefix "${S3_PREFIX}" \
     --query "${backups_query}" \
     --output text \
     | xargs -n1 -t -I 'KEY' aws $aws_args s3 rm s3://"${S3_BUCKET}"/'KEY'
-  echo "Removing complete."
+  echo "Removal complete."
 fi
