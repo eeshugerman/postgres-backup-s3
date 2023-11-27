6079480a2b96aea8957a5e8a0d98e9a905f293c8
Elliott Shugerman <eeshugerman@gmail.com>
2023-11-27 01:13:23 -0500

    add nushell env

---
+++ test/flake.nix
@@ -0,0 +1,14 @@
+{
+  inputs = { flake-utils.url = "github:numtide/flake-utils"; };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nushell
+          ];
+        };
+      });
+}
