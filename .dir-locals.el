d8bd2e4133501a114ca55159a9f7f02d0dcdc8c2
Elliott Shugerman <eeshugerman@gmail.com>
2023-12-24 19:05:35 -0500

    fixup! improvements to janet env

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,4 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((eval . (progn
-                   (setq janet-interpreter-command "jpm")
-                   (setq janet-interpreter-arguments '("-l" "janet" "-s"))))))
- (sh-mode . ((sh-basic-offset . 2))))
+((sh-mode . ((sh-basic-offset . 2))))
