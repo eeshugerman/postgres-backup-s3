97448c8d282ba9223a0a899a95f827591ff74ae2
Elliott Shugerman <eeshugerman@gmail.com>
2023-12-30 13:12:00 -0500

    misc

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,9 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((eval . (advice-add 'inf-janet-project-root
-                             :filter-return (lambda (path)
-                                              (f-join path "test"))))))
+((nil . ((eval .
+               ;; TODO: make this local to the project somehow
+               (advice-add 'inf-janet-project-root
+                           :filter-return (lambda (path)
+                                            (f-join path "test"))))))
  (sh-mode . ((sh-basic-offset . 2))))
